958ad76d29070d7c2edd5620f396a9c1065b7395
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2025-07-14 15:44:10 +0000

    sfd

--- dev.ts
+++ dev.ts
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
+import { tailwind } from "@fresh/plugin-tailwind";
 
 import { Builder } from "fresh/dev";
-import { tailwind } from "@fresh/plugin-tailwind";
 import { app } from "./main.ts";
 
 const builder = new Builder();
 tailwind(builder, app, {});
-
 if (Deno.args.includes("build")) {
   await builder.build(app);
 } else {
