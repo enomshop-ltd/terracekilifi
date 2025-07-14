31717874a8536a8619e0b1c1d3912eba726fb914
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2025-07-14 11:16:46 +0000

    updates

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,12 @@
-import { type PageProps } from "$fresh/server.ts";
-export default function App({ Component }: PageProps) {
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
   return (
     <html>
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>terracekilifi</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <title>Terrace Kilifi</title>
       </head>
       <body>
         <Component />
