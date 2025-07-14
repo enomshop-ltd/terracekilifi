6f8002a37fc3b7c1c5789159a93f6ebd5867f5b2
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2025-07-14 16:53:55 +0000

    dfg

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
             class="w-full" />
           <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" class="btn btn-circle">❮</a>
+            <a href="#slide4" class="btn btn-circle">❮</a>
             <a href="#slide2" class="btn btn-circle">❯</a>
           </div>
         </div>
@@ -29,6 +29,15 @@ export default function Home() {
             class="w-full" />
           <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" class="btn btn-circle">❮</a>
+            <a href="#slide4" class="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide4" class="carousel-item relative w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            class="w-full" />
+          <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" class="btn btn-circle">❮</a>
             <a href="#slide1" class="btn btn-circle">❯</a>
           </div>
         </div>
